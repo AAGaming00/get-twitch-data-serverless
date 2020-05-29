9b4080bf16d39b2b16cbfb793aaa7e81db3408df
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-05-29 13:09:30 -0400

    wwwwwwwwwwww

--- functions/get-stream-data.js
+++ functions/get-stream-data.js
@@ -50,8 +50,9 @@ exports.handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify(response),
       headers: {
-        'content-type': 'application/json',
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':
+          'Origin, X-Requested-With, Content-Type, Accept',
       },
     };
     
